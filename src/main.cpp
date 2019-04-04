671b2347c5574494ae2eee26edcd7ae9b2af6bb7
Kaustubh Welankar <kaustubh0x77@gmail.com>
2019-04-04 16:31:05 +0530

    Change main for testing kps

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "GrahamScan.h"
 #include "JarvisMarch.h"
 #include "ConvexHullGraphix.h"
+#include "KirkpatrickSiedel.h"
+#include "median_finding.cpp"
 
 #define VISUALISE true
 
@@ -27,30 +29,47 @@ int main(){
     }
 
     // TODO: Case fails for Jarvis March. Fix.
-    // Point p1(1,1);
-    // Point p2(2,2);
-    // Point p3(3,3);
-    // Point p4(4,4);
-    // Point p5(5,5);
-    // Point p6(6,6);
+//     Point p1(1,1);
+//     Point p2(2,2);
+//     Point p3(3,3);
+//     Point p4(4,4);
+//     Point p5(5,5);
+//     Point p6(6,6);
+//
+//    Point p1(1,1);
+//    Point p2(0,2);
+//    Point p3(-1,1);
+//    Point p4(-1,-1);
+//    Point p5(0,3);
+//    Point p6(1,-1);
+//
+//    // TODO: Case fails for Graham's Scan. Fix.
+//     Point p1(-6,0);
+//     Point p2(-4,2);
+//     Point p3(0,0);
+//     Point p4(1,5);
+//     Point p5(1.5,3);
+//     Point p6(1.6,1);
+//     Point p7(2,4);
+//     Point p8(6,0);
+//
+//    vector<Point> points;
+//    points.push_back(p1);
+//    points.push_back(p2);
+//    points.push_back(p3);
+//    points.push_back(p4);
+//    points.push_back(p5);
+//    points.push_back(p6);
+//    points.push_back(p7);
+//    points.push_back(p8);
 
-    Point p1(1,1);
-    Point p2(0,2);
-    Point p3(-1,1);
-    Point p4(-1,-1);
-    Point p5(0,3);
-    Point p6(1,-1);
-
-    // TODO: Case fails for Graham's Scan. Fix.
-    // Point p1(-1,1);
-    // Point p2(-0.5,0.5);
-    // Point p3(0,0);
-    // Point p4(-1,-1);
-    // Point p5(1,-1);
-    // Point p6(1,1);
-    // Point p7(2,0);
-    // Point p8(0,-1);
-    
+    Point p1(1,5);
+    Point p2(3,8);
+    Point p3(5,5);
+    Point p4(5,1);
+    Point p5(1,1);
+    Point p6(3,4);
+    Point p7(1.5,4);
     vector<Point> points;
     points.push_back(p1);
     points.push_back(p2);
@@ -58,9 +77,9 @@ int main(){
     points.push_back(p4);
     points.push_back(p5);
     points.push_back(p6);
-    // points.push_back(p7);
-    // points.push_back(p8);
+    points.push_back(p7);
 
+<<<<<<< HEAD
     
     /* Graham's Scan */
     // GrahamScan gh_scan(points);
@@ -73,11 +92,37 @@ int main(){
     jar_march.compute_convex_hull();
     vector<Point> result;
     result = jar_march.get_ch_points();
+=======
 
-    for(auto pt: result)
-        cout << pt << endl;
+//
+//    /* Graham's Scan */
+//    // GrahamScan gh_scan(points);
+//    // gh_scan.compute_convex_hull();
+//    // vector<PolarPoint> result;
+//    // result = gh_scan.get_ch_points();
+//
+//    /* Jarvis March */
+//    JarvisMarch jar_march(points);
+//    jar_march.compute_convex_hull();
+//    vector<Point> result;
+//    result = jar_march.get_ch_points();
+//
+//    for(auto pt: result)
+//        cout << pt << endl;
+//
+//    return 0;
+//
+//    vector<int> a {1,2,3,4};
+//    //for(int i = 0; i < 456262; i++){ a.push_back(i); }
+//    //random_shuffle(a.begin(), a.end());
+>>>>>>> c5bba96... Change main for testing kps
 
-    if(VISUALISE) gfx_thread -> join();
+    KirkpatrickSiedel kps(points);
+    vector<Point> hull = kps.compute();
 
-    return 0;
+    for(auto p : hull){
+        cout << p;
+    }
+    cout << "\n";
+
 }
